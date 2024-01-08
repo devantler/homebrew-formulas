145e2c8ffc0f193840fd68e54669dd15e7c9a2ee
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-01-08 23:50:36 +0100

    ksail 1.0.11
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -1,13 +1,13 @@
 class Ksail < Formula
   desc "Cli tool for provisioning gitops enabled k8s clusters in docker"
   homepage "https://github.com/devantler/ksail"
-  version "1.0.8"
+  version "1.0.11"
   bottle :unneeded
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-osx-arm64.tar.gz"
-      sha256 "replace_with_osx_arm64_sha256"
+      url "https://github.com/devantler/ksail/archive/refs/tags/v1.0.11.tar.gz"
+      sha256 "7e50d40776ac24ed7e2306846bbe6a8760fa994195cb29ab7edc2e4105928bda"
     elsif Hardware::CPU.intel?
       url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-osx-x64.tar.gz"
       sha256 "replace_with_osx_x64_sha256"
