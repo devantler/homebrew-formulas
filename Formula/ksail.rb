c57951e2637be31f789a8e264b575b225ef5b526
devantler <nikolaiemildamm@icloud.com>
2024-01-09 00:00:41 +0100

    Update ksail formula version and remove sha256 checksum

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -2,7 +2,6 @@ class Ksail < Formula
   desc "Cli tool for provisioning gitops enabled k8s clusters in docker"
   homepage "https://github.com/devantler/ksail"
   version "1.0.11"
-  sha256 "7e50d40776ac24ed7e2306846bbe6a8760fa994195cb29ab7edc2e4105928bda"
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-v#{version}-osx-arm64.tar.gz"
