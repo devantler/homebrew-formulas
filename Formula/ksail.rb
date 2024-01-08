8cbbc01da65f07cd436675371f5a3619339c2241
devantler <nikolaiemildamm@icloud.com>
2024-01-09 00:51:13 +0100

    Update version in ksail.rb

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -2,10 +2,10 @@ class Ksail < Formula
   desc "Cli tool for provisioning gitops enabled k8s clusters in docker"
   homepage "https://github.com/devantler/ksail"
   license "MIT"
-  version "1.0.11"
   os = OS.mac? ? "osx" : "linux"
   arch = Hardware::CPU.arm? ? "arm64" : "x64"
   url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-v#{version}-#{os}-#{arch}.tar.gz"
+  version "1.0.11"
   def install
     bin.install "ksail"
   end
