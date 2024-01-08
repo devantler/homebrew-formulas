1e2cafa44849d7f30590f12362b2e6dd5e7f63ef
devantler <nikolaiemildamm@icloud.com>
2024-01-08 23:58:36 +0100

    Update download URLs in ksail.rb

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -5,15 +5,15 @@ class Ksail < Formula
   sha256 "7e50d40776ac24ed7e2306846bbe6a8760fa994195cb29ab7edc2e4105928bda"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-osx-arm64.tar.gz"
+      url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-v#{version}-osx-arm64.tar.gz"
     elsif Hardware::CPU.intel?
-      url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-osx-x64.tar.gz"
+      url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-v#{version}-osx-x64.tar.gz"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-linux-arm64.tar.gz"
+      url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-v#{version}-linux-arm64.tar.gz"
     elsif Hardware::CPU.intel?
-      url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-linux-x64.tar.gz"
+      url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-v#{version}-linux-x64.tar.gz"
     end
   end
 
