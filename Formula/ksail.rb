314acdb0f4e848dfa1bb7514495d4461e35662a3
devantler <nikolaiemildamm@icloud.com>
2024-01-09 01:57:03 +0100

    Update ksail formula with new sha256 checksum for macOS ARM64

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -4,7 +4,7 @@ class Ksail < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/devantler/ksail/releases/download/v1.0.15/ksail-v1.0.15-osx-arm64.tar.gz"
-      sha256 "e2b4b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6"
+      sha256 "deec9f782225274af358d01e31dd50a36d25dde481342505cb230ce0ad06ce69"
     elsif Hardware::CPU.intel?
       url "https://github.com/devantler/ksail/releases/download/v1.0.15/ksail-v1.0.15-osx-x64.tar.gz"
       sha256 "e2b4b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"
