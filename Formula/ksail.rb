04a0f733038af9694a05c99b0c93b35f5f0fc6b3
devantler <nikolaiemildamm@icloud.com>
2024-01-09 01:10:23 +0100

    Update license in ksail.rb

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -4,8 +4,8 @@ class Ksail < Formula
   os = OS.mac? ? "osx" : "linux"
   arch = Hardware::CPU.arm? ? "arm64" : "x64"
   url "https://github.com/devantler/ksail/releases/download/v#{version}/ksail-v#{version}-#{os}-#{arch}.tar.gz"
-  license "MIT"
   version "1.0.11"
+  license "MIT"
   def install
     bin.install "ksail"
   end
