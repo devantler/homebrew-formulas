cf61b13455c15091a88a1468a568776294bbaad3
devantler <nikolaiemildamm@icloud.com>
2024-01-09 02:28:35 +0100

    Update ksail formula to version 1.0.15

--- Formula/ksail.rb
+++ Formula/ksail.rb
@@ -1,27 +1,23 @@
 class Ksail < Formula
   desc "Cli tool for provisioning gitops enabled k8s clusters in docker"
   homepage "https://github.com/devantler/ksail"
+  url "https://github.com/devantler/ksail/archive/v1.0.15.tar.gz"
+  sha256 "deec9f782225274af358d01e31dd50a36d25dde481342505cb230ce0ad06ce69"
   version "1.0.15"
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/devantler/ksail/releases/download/v1.0.15/ksail-v1.0.15-osx-arm64.tar.gz"
-      sha256 "deec9f782225274af358d01e31dd50a36d25dde481342505cb230ce0ad06ce69"
-    elsif Hardware::CPU.intel?
-      url "https://github.com/devantler/ksail/releases/download/v1.0.15/ksail-v1.0.15-osx-x64.tar.gz"
-      sha256 "e2b4b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"
-    end
-  elsif OS.linux?
-    if Hardware::CPU.arm?
-      url "https://github.com/devantler/ksail/releases/download/v1.0.15/ksail-v1.0.15-linux-arm64.tar.gz"
-      sha256 "e2b4b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"
-    elsif Hardware::CPU.intel?
-      url "https://github.com/devantler/ksail/releases/download/v1.0.15/ksail-v1.0.15-linux-x64.tar.gz"
-      sha256 "e2b4b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"
-    end
-  end
-
   license "MIT"
   def install
-    bin.install "ksail"
+    if OS.mac?
+      if Hardware::CPU.arm?
+        bin.install "ksail-v#{version}-osx-arm64" => "ksail"
+      elsif Hardware::CPU.intel?
+        bin.install "ksail-v#{version}-osx-amd64" => "ksail"
+      end
+    elsif OS.linux?
+      if Hardware::CPU.arm?
+        bin.install "ksail-v#{version}-linux-arm64" => "ksail"
+      elsif Hardware::CPU.intel?
+        bin.install "ksail-v#{version}-linux-amd64" => "ksail"
+      end
+    end
   end
 end
